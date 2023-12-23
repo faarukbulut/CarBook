1385d4e75c7d77eb9b45d0c2d599f02cf5ccc2fa
faarukbulut <faarukbulut@gmail.com>
2023-12-23 18:05:12 +0300

    Temanın eklenmesi ve default layout düzenlemeleri

---
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultLayoutViewComponents/_MainDefaultLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.DefaultLayoutViewComponents
+{
+    public class _MainDefaultLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
