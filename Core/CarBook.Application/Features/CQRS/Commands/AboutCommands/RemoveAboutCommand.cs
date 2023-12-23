9ee1c065c2fa79399c7c3975e90b4efb9590a282
faarukbulut <faarukbulut@gmail.com>
2023-12-23 08:01:53 +0300

    About Command Handlerların Yazılması

---
+++ Core/CarBook.Application/Features/CQRS/Commands/AboutCommands/RemoveAboutCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace CarBook.Application.Features.CQRS.Commands.AboutCommands
+{
+    public class RemoveAboutCommand
+    {
+        public RemoveAboutCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
