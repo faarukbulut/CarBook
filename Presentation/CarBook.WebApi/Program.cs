2efef8b1d9fd9d91db1512460129c473ecd59af5
faarukbulut <faarukbulut@gmail.com>
2023-12-23 08:16:28 +0300

    api program.cs yapılandırma

--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -1,4 +1,19 @@
+using CarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBook.Application.Interfaces;
+using CarBook.Persistence.Context;
+using CarBook.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<CarBookContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
 
 
 builder.Services.AddControllers();
