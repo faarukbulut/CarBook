637fb649fbe9fa0d48ceeeb1136623844c7d0618
faarukbulut <faarukbulut@gmail.com>
2023-12-23 07:41:45 +0300

    Repository Sınıfı & Application tekrar düzenleme

--- Core/CarBook.Application/Interfaces/IRepository.cs
+++ Core/CarBook.Application/Interfaces/IRepository.cs
@@ -1,9 +1,9 @@
-﻿namespace Application.Interfaces
+﻿namespace CarBook.Application.Interfaces
 {
     public interface IRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync();
-        Task<T> GetByIdAsync();
+        Task<T> GetByIdAsync(int id);
         Task CreateAsync(T entity);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
