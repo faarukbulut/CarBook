3458f40dbfc5919e305c2309863884f79fe2a389
faarukbulut <faarukbulut@gmail.com>
2023-12-23 07:30:10 +0300

    IRepository Interface

---
+++ Core/Application/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync();
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+    }
+}
