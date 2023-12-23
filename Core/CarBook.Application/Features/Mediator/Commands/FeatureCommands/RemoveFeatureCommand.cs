8f5c5bb36d56985dfc9e0f439065b847f1bd0084
faarukbulut <faarukbulut@gmail.com>
2023-12-23 15:17:18 +0300

    Mediator ile Feature işlemleri

---
+++ Core/CarBook.Application/Features/Mediator/Commands/FeatureCommands/RemoveFeatureCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Commands.FeatureCommands
+{
+    public class RemoveFeatureCommand : IRequest
+    {
+        public RemoveFeatureCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -6,6 +6,7 @@ using CarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
 using CarBook.Application.Features.CQRS.Handlers.ContactHandlers;
 using CarBook.Application.Interfaces;
 using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Application.Services;
 using CarBook.Persistence.Context;
 using CarBook.Persistence.Repositories;
 using CarBook.Persistence.Repositories.CarRepositories;
@@ -53,6 +54,11 @@ builder.Services.AddScoped<GetContactByIdQueryHandler>();
 builder.Services.AddScoped<CreateContactCommandHandler>();
 builder.Services.AddScoped<UpdateContactCommandHandler>();
 builder.Services.AddScoped<RemoveContactCommandHandler>();
+
+
+builder.Services.AddApplicationService(builder.Configuration);
+
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
