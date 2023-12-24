934e611e5315f60f28b76311a025ad4fbd2cc74e
faarukbulut <faarukbulut@gmail.com>
2023-12-24 03:07:53 +0300

    About sayfası API Consume

---
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutTestimonialComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.AboutViewComponents
+{
+	public class _AboutTestimonialComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactor;
+
+		public _AboutTestimonialComponentPartial(IHttpClientFactory httpClientFactor)
+		{
+			_httpClientFactor = httpClientFactor;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactor.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44358/api/Testimonials");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}
---
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactor;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactor)
+        {
+            _httpClientFactor = httpClientFactor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactor.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44358/api/Abouts");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAbutDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
