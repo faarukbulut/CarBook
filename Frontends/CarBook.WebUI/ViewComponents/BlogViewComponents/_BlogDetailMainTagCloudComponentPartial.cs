d3b7a614bfbc011d7534578f0b8eaa9b0d3363c0
faarukbulut <faarukbulut@gmail.com>
2023-12-24 15:34:10 +0300

    blog detay tag cloud listelenmesi

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailMainTagCloudComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailMainTagCloudComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailMainTagCloudComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailMainTagCloudComponentPartial(IHttpClientFactory httpClientFactor)
         {
+            _httpClientFactory = httpClientFactor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44358/api/TagClouds/GetTagCloudByBlogId/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<GetByBlogIdTagCloudDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     } 
