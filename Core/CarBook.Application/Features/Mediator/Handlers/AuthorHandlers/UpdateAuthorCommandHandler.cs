11bc6b17eacfd58c6c225c3eeee020d69a4c08cc
faarukbulut <faarukbulut@gmail.com>
2023-12-24 06:07:02 +0300

    Author Mediator & api

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/AuthorHandlers/UpdateAuthorCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Commands.AuthorCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.AuthorHandlers
+{
+    public class UpdateAuthorCommandHandler : IRequestHandler<UpdateAuthorCommand>
+    {
+        private readonly IRepository<Author> _repository;
+
+        public UpdateAuthorCommandHandler(IRepository<Author> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAuthorCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.AuthorID);
+
+            values.Name = request.Name;
+            values.ImageUrl = request.ImageUrl;
+            values.Description = request.Description;
+
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
---
+++ Core/CarBook.Application/Features/Mediator/Queries/AuthorQueries/GetAuthorByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using CarBook.Application.Features.Mediator.Results.AuthorResults;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Queries.AuthorQueries
+{
+    public class GetAuthorByIdQuery : IRequest<GetAuthorByIdQueryResult>
+    {
+        public GetAuthorByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
