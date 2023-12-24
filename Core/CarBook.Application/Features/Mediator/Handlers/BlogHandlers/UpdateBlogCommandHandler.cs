56042979c737c21e578503389525c4f9c28cb8d2
faarukbulut <faarukbulut@gmail.com>
2023-12-24 06:28:24 +0300

    blog mediator

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/BlogHandlers/UpdateBlogCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Application.Features.Mediator.Commands.BlogCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand>
+    {
+        private readonly IRepository<Blog> _repository;
+
+        public UpdateBlogCommandHandler(IRepository<Blog> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.BlogID);
+
+            values.Title = request.Title;
+            values.AuthorID = request.AuthorID;
+            values.CoverImageUrl = request.CoverImageUrl;
+            values.CreatedDate = request.CreatedDate;
+            values.CategoryID = request.CategoryID;
+
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
