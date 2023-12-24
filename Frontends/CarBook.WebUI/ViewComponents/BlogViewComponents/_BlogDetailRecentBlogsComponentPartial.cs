35e14270abba9b6218a7425a6d45d9fc8f04b830
faarukbulut <faarukbulut@gmail.com>
2023-12-24 14:29:59 +0300

    Blog detay son blog listesi

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailRecentBlogsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailRecentBlogsComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailRecentBlogsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailRecentBlogsComponentPartial(IHttpClientFactory httpClientFactor)
         {
+            _httpClientFactory = httpClientFactor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44358/api/Blogs/GetBlogLast3WithAuthors");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogLast3WithAuthorsDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
