914c8d89f685ab4400d425c293e32871eabb0b0e
faarukbulut <faarukbulut@gmail.com>
2023-12-25 05:50:48 +0300

    Comment Repository

---
+++ Presentation/CarBook.WebApi/Controllers/CommentsController.cs
@@ -0,0 +1,25 @@
+﻿using CarBook.Application.Features.RepositoryPattern;
+using CarBook.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly IGenericRepository<Comment> _commentRepository;
+
+        public CommentsController(IGenericRepository<Comment> commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _commentRepository.GetAll();
+            return Ok(values);
+        }
+    }
+}
