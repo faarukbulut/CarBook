16a08cdae9699373fe3584a267ca921aae2a881c
faarukbulut <faarukbulut@gmail.com>
2023-12-25 06:51:42 +0300

    Admin Layout

---
+++ Frontends/CarBook.WebUI/ViewComponents/AdminLayoutViewComponents/_HeadAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _HeadAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBook.WebUI/ViewComponents/AdminLayoutViewComponents/_ScriptAdminLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _ScriptAdminLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
