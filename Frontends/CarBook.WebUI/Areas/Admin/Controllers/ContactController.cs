0264d43ff26b1938bbf52dfc60578deabaaea422
faarukbulut <faarukbulut@gmail.com>
2023-12-26 14:07:26 +0300

    Admin - İletişim İşlemleri

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44358/api/Contacts");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
