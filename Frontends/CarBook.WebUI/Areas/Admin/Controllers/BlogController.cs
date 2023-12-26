c9807d0d671670822321f868e21eabdfe9461a57
faarukbulut <faarukbulut@gmail.com>
2023-12-26 11:46:08 +0300

    Admin - Blog Listeleme

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using CarBook.Dto.AuthorDtos;
+using CarBook.Dto.BlogDtos;
+using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class BlogController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public BlogController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44358/api/Blogs");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultBlogDto>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+
+		public async Task<IActionResult> RemoveBlog(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync($"https://localhost:44358/api/Blogs/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+	}
+}
