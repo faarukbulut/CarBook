3197cdd2ee77071693537e0b5fd61e879ab05119
faarukbulut <faarukbulut@gmail.com>
2024-01-04 15:05:17 +0300

    Blog yorumlarını getirme

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailCommentsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailCommentsComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailCommentsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailCommentsComponentPartial(IHttpClientFactory httpClientFactor)
         {
+            _httpClientFactory = httpClientFactor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44358/api/Comments/CommentListByBlog/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
