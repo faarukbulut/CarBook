9e662d0b2052b6fee5b5bd75d323842b4d6197e8
faarukbulut <faarukbulut@gmail.com>
2024-01-04 14:43:05 +0300

    Footer işlemleri

--- Frontends/CarBook.WebUI/ViewComponents/DefaultLayoutViewComponents/_FooterDefaultLayoutComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultLayoutViewComponents/_FooterDefaultLayoutComponentPartial.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.DefaultLayoutViewComponents
 {
     public class _FooterDefaultLayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }
-    }
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _FooterDefaultLayoutComponentPartial(IHttpClientFactory httpClientFactor)
+		{
+			_httpClientFactory = httpClientFactor;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44358/api/FooterAddresses");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+	}
 }
