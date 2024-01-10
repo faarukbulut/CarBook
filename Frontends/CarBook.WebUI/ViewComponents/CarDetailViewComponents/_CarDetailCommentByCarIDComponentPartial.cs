fff8cac8583c831ca2b24be7cd5ceea99518d190
faarukbulut <faarukbulut@gmail.com>
2024-01-11 01:23:04 +0300

    Car Detail Yorumlar View Component

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentByCarIDComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailCommentByCarIDComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
