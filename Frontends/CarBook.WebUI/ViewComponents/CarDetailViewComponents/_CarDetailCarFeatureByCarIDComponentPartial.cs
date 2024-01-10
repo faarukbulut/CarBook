de3af25d98a51a1f6fb6a816fd9fbc0f46c6b507
faarukbulut <faarukbulut@gmail.com>
2024-01-10 18:45:58 +0300

    Araba Özelliklerinin UI'da listelenmesi

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCarFeatureByCarIDComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Dto.CarFeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+	public class _CarDetailCarFeatureByCarIDComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarDetailCarFeatureByCarIDComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"https://localhost:44358/api/CarFeatures/{id}");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIDDto>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}
