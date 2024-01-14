7aff650acb443d7c9f0550ff0163d5108d93acf5
faarukbulut <faarukbulut@gmail.com>
2024-01-14 17:59:22 +0300

    Review UI Listeleme

--- Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentByCarIDComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailCommentByCarIDComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.ReviewDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
 {
 	public class _CarDetailCommentByCarIDComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _CarDetailCommentByCarIDComponentPartial(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"https://localhost:44358/api/Reviews/{id}");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultReviewByCarIdDto>>(jsonData);
+				return View(values);
+			}
+
 			return View();
 		}
 	}
