b427ad5de6f78008ef98ad66126c2d6d6d3b1251
faarukbulut <faarukbulut@gmail.com>
2024-01-16 11:29:47 +0300

    App User Create Handler

---
+++ Core/CarBook.Application/Features/Mediator/Commands/AppUserCommands/CreateAppUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Commands.AppUserCommands
+{
+	public class CreateAppUserCommand : IRequest
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
---
+++ Core/CarBook.Application/Features/Mediator/Handlers/AppUserHandlers/CreateAppUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Application.Features.Mediator.Commands.AppUserCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+	public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommand>
+	{
+		private readonly IRepository<AppUser> _repository;
+
+		public CreateAppUserCommandHandler(IRepository<AppUser> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
+		{
+			await _repository.CreateAsync(new AppUser
+			{
+				Username = request.Username,
+				Password = request.Password,
+				AppRoleID = 2,
+			});
+		}
+	}
+}
