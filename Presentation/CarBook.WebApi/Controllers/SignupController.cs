9ebaa1c5c8d4271d43a8d8c11494e50da687c72e
faarukbulut <faarukbulut@gmail.com>
2024-01-16 11:34:54 +0300

    register api controller

---
+++ Presentation/CarBook.WebApi/Controllers/SignupController.cs
@@ -0,0 +1,25 @@
+﻿using CarBook.Application.Features.Mediator.Commands.AppUserCommands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SignupController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public SignupController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(CreateAppUserCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Kullanıcı başarıyla oluşturuldu");
+		}
+	}
+}
